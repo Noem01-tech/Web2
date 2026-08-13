e8f790867f9669e9445a61cc2ce2902bbe432426
Andriampenomanana Fenohasina <hei.fenohasina.17@gmail.com>
2026-08-13 19:18:44 +0300

    feat: finalisation de l'implementation de la couche service

--- src/Service/studentService.ts
+++ src/Service/studentService.ts
@@ -1,4 +1,4 @@
-import {Student,type CreateStundent} from "../Model/Student";
+import {Student, CreateStudent} from "../Model/Student";
 import {findAll,
         findById,
         create,
@@ -6,8 +6,22 @@ import {findAll,
         remove
 } from "../Repository/studentRepository";
 
-export class StudentService{
-    async getAllStudents(): Promise<Student>{
-        return await findAll();
-    }
+export async function getStudents(): Promise<Student[]>{
+    return findAll();
+}
+
+export async function getStudentById(id:number):Promise<Student | null>{
+    return findById(id);
+}
+
+export async function createStudent(student: CreateStudent): Promise<Student>{
+    return create(student);
+}
+
+export async function updateStudent(id:number,student:CreateStudent):Promise<Student| null>{
+    return update(id,student);
+}
+
+export async function deleteStudent(id:number):Promise<Student|null>{
+    return remove(id);
 }
